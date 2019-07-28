3fc8c8f46ebc5a0e472e8ecdb6fc418d5cef9360
Tobias Trein <treintobias47@gmail.com>
2019-07-28 15:23:56 -0300

    config do app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // App
 const app = express();
+require('dotenv').config();
+
+// Database
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+  
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', err => {
+    console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log(
+        'Mongoose default connection is disconnected due to application termination'
+        );
+        process.exit(0);
+    });
+});
+
+
 
 // Load routes
 const indexRoutes = require('./routes/index-routes');
